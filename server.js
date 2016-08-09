fa2eb9064c1b4214515063d025e3a12cc8a9c653
ramachandran <ramachandran@weinvoiceit.com>
2016-08-09 11:11:43 +0530

    index page rendered using express server.

---
+++ server.js
@@ -0,0 +1,30 @@
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+const express = require('./node_modules/react-scripts/node_modules/express');
+const app = express();
+const path = require('path');
+
+
+const PORT = process.env.PORT || 5000;
+
+throng({
+    workers: WORKERS,
+    grace: 1000,
+    lifetime: Infinity,
+    start: startFunction
+});
+
+function startFunction() {
+
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.set('port', PORT);
+
+    app.get('/', function (req, res) {
+        res.render("index.html");
+    });
+
+    app.listen(app.get('port'), function () {
+        console.log('Heroku usage archive application running...');
+    });
+
+}
