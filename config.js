78f21b3ffabd29e4758027f0f88546f148645d90
rama <r.venkatachalam@salesforce.com>
2016-08-17 09:57:00 +0530

    modified app.json and config files.

--- config.js
+++ config.js
@@ -3,20 +3,20 @@ const config = {
         level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'development' ? 'debug' : 'info')
     },
     database: {
-        pgUri: process.env.DATABASE_URL || 'postgres://localhost:5432/salesforce'
+        pgUri: process.env.DATABASE_URL
     },
     server: {
         port: process.env.PORT || 5000
     },
     salesforce: {
         appName: process.env.HEROKU_APP_NAME,
-        user: process.env.SALESFORCE_USER || 'ramac@billing.lightning.com',
-        pass: process.env.SALESFORCE_PASSWORD || 'Rama_2016',
-        token: process.env.SALESFORCE_TOKEN || 'oLhTN81i2ehWJ3pO5J3fqwodN',
+        user: process.env.SALESFORCE_USER,
+        pass: process.env.SALESFORCE_PASSWORD,
+        token: process.env.SALESFORCE_TOKEN,
         namespace: process.env.SF_PKG_NAMESPACE || 'Invoice_it__',
         archiveObject: process.env.ARCHIVE_OBJECT || ['Usage_Charge__c'],
-        archiveRule: process.env.ARCHIVE_RULE,
-        cronTime: process.env.CRON_TIME
+        archiveRule: process.env.ARCHIVE_RULE || "'status__c'='Completed'",
+        cronTime: process.env.CRON_TIME || '0 0 * * * *'
     },
     meta: {
         env: process.env.NODE_ENV || 'development'
